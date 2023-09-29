88485ffd328854a743adcba54c4ababc5c51feaa
Lorenzo Delgado <lnsdev@proton.me>
2023-09-29 13:24:50 +0200

    feat(bake-kas): integration tests tempdir fixture

--- bake-kas/tests/it_kas_checkout.rs
+++ bake-kas/tests/it_kas_checkout.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use bake_kas::kas::KasContextBuilder;
 
@@ -12,16 +12,61 @@ fn tests_output_dir() -> PathBuf {
     PathBuf::from(env!("OUT_DIR")).join("tests")
 }
 
-/// Convenience function to get the path to the tests output temporary directory.
-fn test_tempdir(dir: &str) -> PathBuf {
-    let tmp = tests_output_dir().join("tmp").join(dir);
+pub struct TempDir(PathBuf);
 
-    // If the directory doesn't exist, create it.
-    if !tmp.exists() {
-        std::fs::create_dir_all(&tmp).expect("Failed to create temporary directory");
+impl TempDir {
+    /// Create the temporary directory if it doesn't exist.
+    pub fn init(&self) -> std::io::Result<()> {
+        if !self.0.exists() {
+            return std::fs::create_dir_all(&self.0);
+        }
+
+        Ok(())
     }
+}
 
-    tmp
+impl From<PathBuf> for TempDir {
+    fn from(path: PathBuf) -> Self {
+        Self(path)
+    }
+}
+
+impl AsRef<Path> for TempDir {
+    fn as_ref(&self) -> &Path {
+        &self.0
+    }
+}
+
+impl std::ops::Deref for TempDir {
+    type Target = PathBuf;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::DerefMut for TempDir {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        if self.0.exists() {
+            std::fs::remove_dir_all(&self.0).expect("Failed to remove temporary directory");
+        }
+    }
+}
+
+/// Convenience function to get the path to the tests output temporary directory.
+fn test_tempdir(dir: &str) -> TempDir {
+    let tmpdir_path = tests_output_dir().join("tmp").join(dir);
+
+    // If the temporary directory doesn't exist, create it.
+    let tmpdir = TempDir::from(tmpdir_path);
+    tmpdir.init().expect("Failed to create temporary directory");
+    tmpdir
 }
 
 /// Convenience function to get the path to a test asset.
